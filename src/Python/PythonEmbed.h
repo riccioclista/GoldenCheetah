8651c3427df7867564ea442f06a80fe9398242d6
Antonius Riha <antoniusriha@gmail.com>
2018-11-18 10:05:51 +0100

    Make python fixes first class data processors

--- src/Python/PythonEmbed.h
+++ src/Python/PythonEmbed.h
@@ -36,12 +36,15 @@ extern PythonEmbed *python;
 class ScriptContext {
     public:
 
-        ScriptContext(Context *context=NULL, RideItem *item=NULL, const QHash<QString,RideMetric*> *metrics=NULL, Specification spec=Specification(),
+        ScriptContext(Context *context=NULL, RideItem *item=NULL, RideFile *rideFile = NULL,
+                      const QHash<QString,RideMetric*> *metrics=NULL, Specification spec=Specification(),
                       bool readOnly = true, QList<RideFile *> *editedRideFiles = NULL)
-            : context(context), item(item), metrics(metrics), spec(spec), readOnly(readOnly), editedRideFiles(editedRideFiles) {}
+            : context(context), item(item), rideFile(rideFile), metrics(metrics), spec(spec),
+              readOnly(readOnly), editedRideFiles(editedRideFiles) {}
 
         Context *context;
         RideItem *item;
+        RideFile *rideFile;
         const QHash<QString,RideMetric*> *metrics;
         Specification spec;
 
